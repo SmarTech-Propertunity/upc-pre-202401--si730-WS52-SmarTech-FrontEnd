e812ae25092b10f3575d959402e7c5386b22d037
RubDaShen <mallmaquisperuben024@gmail.com>
2024-04-30 14:46:00 -0500

    feat: added inicio-home

---
+++ src/public/components/optionBlock.component.vue
@@ -0,0 +1,71 @@
+<script>
+export default
+{
+  name: "option-block",
+
+  props: {
+    imagePath: {
+      type: String,
+      required: true
+    },
+    imageAlt: {
+      type: String,
+      required: true
+    },
+    buttonLabel: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    imageWidth: {
+      type: Number
+    },
+    imageHeight: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="block-object">
+    <img :src="imagePath" :alt="imageAlt" :height=imageWidth :width=imageHeight>
+    <div class="block-description">
+      <p style="padding-bottom: 1rem;">{{description}}</p>
+      <pv-button class="option-button" :label="buttonLabel"></pv-button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.block-object {
+  align-items: center;
+  display: flex;
+  width: 26rem;
+  height: 8rem;
+  background: #ffa07a;
+  padding: 1rem;
+  border: 0.25rem solid #2c3e50;
+  gap: 2.5rem;
+  padding-left: 2.5rem;
+}
+.block-description {
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 0.85rem;
+  width: 14rem;
+  line-height: 1.15rem;
+}
+.option-button {
+  border: 0 solid #ffffff;
+  font-size: 1.0rem;
+  background-color: #ffcc00;
+  border-radius: 1.0rem;
+  padding: 0.5rem 2.8rem;
+}
+.option-button:hover {
+  background-color: #ffffa0;
+}
+</style>
---
+++ src/public/components/theOptions.component.vue
@@ -0,0 +1,42 @@
+<script>
+import OptionBlock from "@/public/components/optionBlock.component.vue";
+
+export default
+{
+  name: "the-options",
+  components: {OptionBlock},
+}
+</script>
+
+<template>
+  <div class="search-agency">
+    <option-block
+        button-label="Buscar"
+        image-alt="Evaluación crediticia"
+        image-path="src/assets/images/saved-icon.png"
+        description="Una evaluación crediticia le permitirá analizar el riesgo crediticio sobre la adquisición de un inmueble"
+        image-width=75
+        image-height=50
+    ></option-block>
+
+    <option-block
+        button-label="Evalúe ahora"
+        image-alt="Buscar inmobiliaria"
+        image-path="src/assets/images/edit-icon.png"
+        description="Una evaluación crediticia le permitirá analizar el riesgo crediticio sobre la adquisición de un inmueble"
+        image-width=75
+        image-height=75
+    ></option-block>
+  </div>
+</template>
+
+<style scoped>
+  .search-agency {
+    background: #f0f0f0;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5rem;
+  }
+</style>
