b0638e520e412ea09f7e85a535e741de9fdbf743
Aldhair Johan Juan Valenzuela Huillcaya <149197456+AldhaValenzuelaH@users.noreply.github.com>
2024-04-29 23:35:56 -0500

    feat: add login, recovery password and register

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
-
+import Login from "@/public/login.component.vue";
+import RecoveryPassword from "@/public/recovery-password.component.vue";
+import Register from "@/public/register.component.vue";
 </script>
 
 <template>
+  <Login/>
+  <RecoveryPassword/>
+  <Register/>
 
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import './assets/styles/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+//import router from './router'
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css"
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
+import Button from 'primevue/button';
+
 
 const app = createApp(App)
 
-app.use(router)
-
+//app.use(router)
+app.use(PrimeVue);
+app.component('pv-InputText', InputText);
+app.component('pv-floatLabel', FloatLabel);
+app.component('pv-button', Button);
 app.mount('#app')
+
