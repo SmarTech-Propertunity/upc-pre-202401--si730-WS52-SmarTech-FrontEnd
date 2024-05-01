0135961d9e01a7d14420bf3fe190fdf422af3a02
RubDaShen <mallmaquisperuben024@gmail.com>
2024-05-01 15:42:36 -0500

    fix: suppressed and fixed warnings

--- src/core/public/components/home/the-home.component.vue
+++ src/core/public/components/home/the-home.component.vue
@@ -4,14 +4,12 @@ import TheSearchAndFilter from "@/core/public/components/home/the-search.compone
 import TheOptions from "@/core/public/components/home/the-options.component.vue";
 import TheFeaturedPublications from "@/core/public/components/home/the-featured-publications.component.vue";
 
-
-
-
 export default {
   name: "the-home-component",
   components: {Toolbar, TheSearchAndFilter, TheOptions, TheFeaturedPublications},
 }
 </script>
+
 <template>
 
   <Toolbar></Toolbar>
