80be519ff120dd0fbc907f399839241b43a2ad2d
Lucio Yen <U202213143@upc.edu.pe>
2024-05-01 04:36:30 -0500

    feat: fixed home-component

--- src/core/public/components/home/the-home.component.vue
+++ src/core/public/components/home/the-home.component.vue
@@ -1,5 +1,17 @@
-<script/>
+<script>
+import Toolbar from "@/core/public/components/home/the-toolbar.component.vue";
+import TheSearchAndFilter from "@/core/public/components/home/the-search.component.vue";
+import TheOptions from "@/core/public/components/home/the-options.component.vue";
+import TheFeaturedPublications from "@/core/public/components/home/the-featured-publications.component.vue";
 
+
+
+
+export default {
+  name: "the-home-component",
+  components: {Toolbar, TheSearchAndFilter, TheOptions, TheFeaturedPublications},
+}
+</script>
 <template>
 
   <Toolbar></Toolbar>
