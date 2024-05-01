3ffb09298867e7022a952da040ec99a1f6b456fe
RubDaShen <mallmaquisperuben024@gmail.com>
2024-05-01 15:32:54 -0500

    fix: suppressed and fixed warnings

---
+++ src/core/public/components/home/the-home.component.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TheToolbar from "@/core/public/components/home/the-toolbar.component.vue";
+import TheSearchAndFilter from "@/core/public/components/home/the-search.component.vue";
+import TheOptions from "@/core/public/components/home/the-options.component.vue";
+import TheFeaturedPublications from "@/core/public/components/home/the-featured-publications.component.vue";
+</script>
+
+<template>
+
+  <the-toolbar></the-toolbar>
+  <the-search-and-filter></the-search-and-filter>
+  <the-options></the-options>
+  <the-featured-publications></the-featured-publications>
+
+</template>
+
+<style scoped>
+
+</style>
