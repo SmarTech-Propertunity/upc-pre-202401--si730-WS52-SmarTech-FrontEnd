cdc1f27b1f66704b6ffcc8ba2f4e2af99aa9e33d
RubDaShen <mallmaquisperuben024@gmail.com>
2024-04-30 23:55:36 -0500

    fix: suppressed and fixed warnings

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-
+import TheToolbar from "@/public/components/theToolbar.component.vue";
+import TheOptions from "@/public/components/theOptions.component.vue";
+import TheFeaturedPublications from "@/public/components/theFeaturedPublications.component.vue";
+import TheSearchAndFilter from "@/public/components/theSearchAndFilters.component.vue";
 </script>
 
 <template>
-
+  <the-toolbar></the-toolbar>
+  <the-search-and-filter></the-search-and-filter>
+  <the-options></the-options>
+  <the-featured-publications></the-featured-publications>
 </template>
 
 <style scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
-import './assets/styles/main.css'
+//  'Router' is not defined yet for use.
+//  All add it later :]
 
+import '@/assets/styles/main.css'
+import PrimeVue from "primevue/config";
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import Toolbar from "primevue/toolbar";
+import Button from "primevue/button";
+import Avatar from "primevue/avatar";
+import InputText from "primevue/inputtext";
+import FloatLabel from "primevue/floatlabel";
+import Dropdown from "primevue/dropdown";
+import Card from "primevue/card";
+//  import router from './router'
+
+
 
 const app = createApp(App)
-
-app.use(router)
-
+app.use(PrimeVue, {unstyled: true })
+    .component('pv-toolbar', Toolbar)
+    .component('pv-button', Button)
+    .component('pv-avatar', Avatar)
+    .component('pv-input-text', InputText)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-dropdown', Dropdown)
+    .component('pv-card', Card)
+//  app.use(router)
 app.mount('#app')
